2f34611b4fb748438b6dc31d783c91493ea0228d
shounak03 <shounakmis.3@gmail.com>
2025-04-26 13:28:53 +0530

    fixed prod errors

--- components/AppWalletProvider.tsx
+++ components/AppWalletProvider.tsx
@@ -8,6 +8,7 @@ import {
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { clusterApiUrl } from "@solana/web3.js";
+
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 
