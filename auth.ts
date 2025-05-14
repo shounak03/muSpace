0776ef1cf8f63a71ffdc9c13bf9ee8276c15f0c0
shounak03 <shounakmis.3@gmail.com>
2025-05-14 10:01:07 +0530

    google auth fix

--- auth.ts
+++ auth.ts
@@ -13,6 +13,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        redirect_uri: process.env.NEXTAUTH_URL + '/api/auth/callback/google'
+      }
     }),
     Credentials({
       id: "credentials",
